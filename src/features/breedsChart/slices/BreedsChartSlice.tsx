c68f39efe5a424d395d0651f34dd1309bed77b89
neuroflip <jaanguita@MacBook-Pro-de-Jose.local>
2025-04-03 11:32:02 +0200

    Corrected the store reducers and added a tests for the initial actions dispatch

--- src/features/breedsChart/slices/BreedsChartSlice.tsx
+++ src/features/breedsChart/slices/BreedsChartSlice.tsx
@@ -14,7 +14,7 @@ const fetchBreeds = createAsyncThunk('breedsChart/fetchBreeds', async (_, thunkA
   const data = await response.json();
 
   checkResponseAndThrowErrorIfNeedded(response.status, data, thunkApi.rejectWithValue);
-    const result = Object.keys(data.message).map(async (breedName: string) => {
+  const result = Object.keys(data.message).map(async (breedName: string) => {
     const response = await fetch(getFetchImageAPIUrl(breedName));
     const data = await response.json();
 
@@ -33,19 +33,22 @@ const breedsChartSlice = createSlice({
   name: "breedsChart",
   initialState,
   reducers: {
-    fetchBreeds(state) {
+    initBreeds(state) {
       state.isLoading = true;
       state.error = null;
+      state.breeds = [];
     }
   },
   extraReducers: builder => {
     builder
       .addCase(fetchBreeds.pending, (state) => {
         state.isLoading = true
+        state.error = null;
       })
       .addCase(fetchBreeds.fulfilled, (state, action) => {
         state.breeds = action.payload;
         state.isLoading = false;
+        state.error = null;
       })
       .addCase(fetchBreeds.rejected, (state, action) => {
         state.error = action.payload as string || "Failed to fetch breeds";
